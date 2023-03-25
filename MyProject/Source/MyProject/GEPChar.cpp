76feed50ee29599d007358cda19dbb6eed04cea8
Nicholas Deli <92155926+NFDELI@users.noreply.github.com>
2023-03-25 21:16:27 +0000

    Finished Week 4 Part 1 (Second Time)

---
+++ MyProject/Source/MyProject/GEPChar.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "GEPChar.h"
+
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+AGEPChar::AGEPChar()
+{
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.f);
+
+	_Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	_Camera->SetupAttachment(GetCapsuleComponent());
+	_Camera->SetRelativeLocation(FVector(-10.f, 0.f, 60.f));
+	_Camera->bUsePawnControlRotation = true;
+
+	_WeaponAttachPoint = CreateDefaultSubobject<USceneComponent>(TEXT("WeaponAttachPoint"));
+	_WeaponAttachPoint->SetupAttachment(_Camera);
+}
+
+void AGEPChar::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGEPChar::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AGEPChar::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if(Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AGEPChar::Shoot()
+{
+}
+
+void AGEPChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if(UEnhancedInputComponent* EnchancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnchancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnchancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		EnchancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGEPChar::Move);
+		
+		EnchancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AGEPChar::Look);
+		
+		EnchancedInputComponent->BindAction(ShootAction, ETriggerEvent::Triggered, this, &AGEPChar::Shoot);
+	}
+}
